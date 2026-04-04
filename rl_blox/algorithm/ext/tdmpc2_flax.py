a4f548c608387be735872ce4563871794c4b58ed
Adrian Schucht <schucht@uni-bremen.de>
2026-04-04 15:10:18 +0200

    refactor(tdmpc2): remove int_to_one_hot (unused)

--- rl_blox/algorithm/ext/tdmpc2_flax.py
+++ rl_blox/algorithm/ext/tdmpc2_flax.py
@@ -686,16 +686,6 @@ def squash(
     return mu, pi, log_pi
 
 
-def int_to_one_hot(x, num_classes):
-    """
-    Converts an integer tensor to a one-hot tensor.
-    Supports batched inputs.
-    """
-    one_hot = torch.zeros(*x.shape, num_classes, device=x.device)
-    one_hot.scatter_(-1, x.unsqueeze(-1), 1)
-    return one_hot
-
-
 @jax.jit
 def symlog(x: ArrayLike) -> Array:
     """
